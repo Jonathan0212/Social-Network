3f672adbba41c7869af3c6a32acd146bb2249913
Jonathan Martinez <jon@jonathans-mbp.attlocal.net>
2022-04-16 19:28:13 -0500

    added schema methods to User.js

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model} = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        } 
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,  
+        },
+        id:false
+    }
+);
+
+UserSchema.virtual('thoughtCount').get(function() {
+    return this.thoughts.reduce((total, thought) => total + thought.reactions.length + 1, 0);
+}); 
+
+const User = model('User', UserSchema);
+
+module.exports = User;
