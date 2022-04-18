f193e39bedd4a2b383ec45ce48f9f944f1bd93b5
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-04-17 20:30:28 -0500

    get all users and get UserById methods are written have not debugged yet

---
+++ controllers/user-controller.js
@@ -0,0 +1,39 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUser(req, res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select:'-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+
+    getUserById({ param}, res) {
+        User.findOne({ _id: URLSearchParams.id })
+        .populate({
+            path:'thoughts',
+            select:'-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this ID'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+}
