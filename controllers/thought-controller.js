d0358a3efc597f7bb241748e4182a1b20d4dcdb1
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-04-17 20:15:38 -0500

    all methods including adding and removing reaction. Remove thought and update thought methods are added.

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,5 +1,6 @@
 const { Thought, User } = require('../models');
 
+// Object with add thought to user method on line 5
 const thoughtController = {
     addThought ({ params, body}, res) {
         console.log(body);
@@ -21,7 +22,7 @@ const thoughtController = {
         .catch(err => res.json(err));
     },
 
-
+// Add Reaction Method
     addReaction ({ params, body}, res) {
         Thought.findOneAndUpdate (
             { _id: params.thoughtId}, 
@@ -36,4 +37,59 @@ const thoughtController = {
             })
             .catch(err => res.json(err));
     },
-}
+
+// Remove Reaction Method
+    removeReaction ({ params}, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: {reactions: {reactionId: params.reactionId } } },
+            { new: true }
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json (err));
+    },
+
+// Remove Thought Method
+    removeThought ({ params }, res) {
+        Thought.findOneAndDelete ({ _id: params.thoughtId})
+        .then(deletedThought => {
+            if(!deletedThought) {
+                return res.status(404).json({ message: 'No thought found with this id'});
+            }
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $pull: { thoughts: params.thoughtId } },
+                { new: true} 
+         );
+    })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json ({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },    
+
+ // Update Thought Method    
+    updateThought ({ params, body }, res){
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $set: body },
+            { new: true }
+        )
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    }
+};
+
+      
+
+
