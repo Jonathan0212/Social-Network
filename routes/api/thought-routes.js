ff8f8e63b467d36a5bbd4d336d20411640855825
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-04-17 23:25:00 -0500

    added routes for thoughts in thought-controller.js

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {
+    addThought,
+    removeThought,
+    addReaction,
+    updateThought,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts/{userId}
+router
+    .route('/:userId')
+    .post(addThought);
+
+// /api/thoughts/{userId}/{thoughtId}
+router
+    .route('/:thoughtId')
+    .put(updateThought)
+    .delete(removeThought);
+
+// /api/thoughts/{thoughtsId}/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/thoughts/{userId}/{thoughtId}
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
+
+module.exports = router;
